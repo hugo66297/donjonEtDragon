da23b43ba8709849e2b72bc3efa714c4e4ed1bfe
Samoht70 <tpoirey70@gmail.com>
2022-10-30 14:01:09 +0100

    ajout d'un user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,8 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'pseudo' => 'admin',
+            'password' => '$2y$10$Epx4bvPBGRlKf18k4qUs5.9EGkA2p5JIBYOvo2YiXCqvR/DqZkpP2', // password
             'remember_token' => Str::random(10),
         ];
     }
