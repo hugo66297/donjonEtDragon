036a2ee5dc794d47a4b9bb00c5d5503b1f6a554e
Samoht70 <tpoirey70@gmail.com>
2022-11-07 21:24:50 +0100

    modif page perso

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,8 +12,8 @@
         <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
 
         <!-- Scripts -->
+{{--        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>--}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen flex flex-col">
