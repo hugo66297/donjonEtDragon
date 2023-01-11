e8b5137fd829423d00b0020fe0c640dc0f9dd827
hugo66297 <hugomater@orange.fr>
2023-01-11 23:49:33 +0200

    skill + table d jointure

---
+++ app/Models/AbilitySkill.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AbilitySkill extends Model
+{
+    use HasFactory;
+}
