c42ddfa6330fcff29352c2c3b79cd4a5d01bfa94
hugo66297 <hugomater@orange.fr>
2023-01-14 18:15:55 +0200

    feed subrace, attack & weapons DATA BASE FULLY FILL

--- database/migrations/2022_11_07_185545_create_weapons_table.php
+++ database/migrations/2022_11_07_185545_create_weapons_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Weapon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,55 @@ return new class extends Migration
             $table->string('damage_type',191);
             $table->text('sub_info')->nullable();
         });
+        Weapon::insert([
+            'name' => 'Hache à deux mains',
+            'atk_bonus'=> '+5',
+            'damage_type' => '1d12 + 3 tranchants'
+        ]);
+        Weapon::insert([
+            'name' => 'Javeline',
+            'atk_bonus'=> '+5',
+            'damage_type' => '1d6 + 3 perforants',
+            'sub_info'=>'Vous pouvez lancer une javeline à une distance de 9 mètres, ou jusqu\'à 36 mètres en étant désavantagé lors du jet d\'attaque.'
+        ]);
+        Weapon::insert([
+            'name' => 'Marteau de guerre',
+            'atk_bonus'=> '+4',
+            'damage_type' => '1d8 + 2 contondants'
+        ]);
+        Weapon::insert([
+            'name' => 'Hachette',
+            'atk_bonus'=> '+4',
+            'damage_type' => '1d6 + 2 tranchants',
+            'sub_info'=>'Vous pouvez lancer une hachette à une distance de 6 mètres ou jusqu\'à 18 mètres en étant désavantagé lors du jet d\'attaque.'
+        ]);
+        Weapon::insert([
+            'name' => 'Épée courte',
+            'atk_bonus'=> '+5',
+            'damage_type' => '1d6 + 3 perforants'
+        ]);
+        Weapon::insert([
+            'name' => 'Arc court',
+            'atk_bonus'=> '+5',
+            'damage_type' => '1d6 + 3 perforants',
+            'sub_info'=>'Vous pouvez tirer avec votre arc court à une distance de 24 mètres, ou jusqu\'à 96 mètres en étant désavantagé lors du jet d\'attaque.'
+        ]);
+        Weapon::insert([
+            'name' => 'Épée courte',
+            'atk_bonus'=> '+4',
+            'damage_type' => '1d6 + 2 perforants'
+        ]);
+        Weapon::insert([
+            'name' => 'Épée à deux mains',
+            'atk_bonus'=> '+4',
+            'damage_type' => '2d6 + 2 tranchants'
+        ]);
+        Weapon::insert([
+            'name' => 'Arc long',
+            'atk_bonus'=> '+7',
+            'damage_type' => '1d8 + 3 perforants',
+            'sub_info'=>'Vous pouvez utiliser votre arc long pour tirer jusqu\'à une distance de 45 mètres ou jusqu\'à 180 mètres en étant désavantagé lors du jet d\'attaque.'
+        ]);
     }
 
     /**
