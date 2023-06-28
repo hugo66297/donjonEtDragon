86f1dd988bbc0dbda0ece5f530421f6b77935a92
Samoht70 <t.poirey@xefi.fr>
2023-06-28 09:38:12 +0200

    changement pour la mise en prod

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -16,7 +16,13 @@
             <div>
                 <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                <x-input
+                    id="email"
+                    type="email"
+                    name="email"
+                    required
+                    autofocus
+                />
 
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
@@ -25,7 +31,12 @@
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+                <x-input
+                    id="password"
+                    type="password"
+                    name="password"
+                    required
+                />
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
@@ -34,9 +45,12 @@
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
+                <x-input
+                    id="password_confirmation"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                />
 
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
