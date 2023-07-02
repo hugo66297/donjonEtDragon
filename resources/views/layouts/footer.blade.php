5cdef7d0b9912f536a4ef6d374bf6eba49f20c4c
hugo66297 <hugomater@orange.fr>
2023-07-02 10:11:21 +0200

    footer updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,19 +2,13 @@
 
 <footer class="shadow p-5 bg-gray-800 {{$display}} z-50 flex-col items-center justify-center md:flex-row md:justify-between">
     <span class="text-sm sm:text-center text-gray-400 flex flex-wrap items-center justify-center">
-        © 2022
-        <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>
-        . All Rights Reserved.
+        © 2022-2023
+        <a href="https://flowbite.com/" class="hover:underline"></a>
+        . Hugo & Thomas
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0 justify-center">
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+            <a href="https://flowbite.com/" class="mr-4 hover:underline md:mr-6 ">Made with Flowbite™</a>
         </li>
         <li>
             <a href="#" class="hover:underline">Contact</a>
